b534978436f22c0bf1a3e20baba88a357f11a880
webgraph-db <webgraphdb@gmail.com>
2026-07-22 00:06:43 +0545

    new changes

--- src/components/Genealogy/Genealogy.jsx
+++ src/components/Genealogy/Genealogy.jsx
@@ -1,32 +1,63 @@
 import "./Genealogy.css";
-import familyTree from "../../data/familyData.js";
+import familyMembers from "../../data/familydata-temp";
 import TreeNode from "./TreeNode";
 
+function buildTree(data) {
+  const map = {};
+
+  data.forEach(person => {
+    map[person.id] = {
+      ...person,
+      children: []
+    };
+  });
+
+  let root = null;
+
+  data.forEach(person => {
+    if (person.parentId === null) {
+      root = map[person.id];
+    } else {
+      map[person.parentId].children.push(map[person.id]);
+    }
+  });
+
+  return root;
+}
+
 export default function Genealogy() {
+
+  const familyTree = buildTree(familyMembers);
+
   return (
     <section className="genealogy section" id="genealogy">
+
       <div className="container">
 
         <div className="section-header">
+
           <span className="section-tag">वंशावली</span>
- 
 
           <h2 className="section-title">
             जुम्क समुदायको वंशावली
           </h2>
 
           <p className="section-subtitle">
-            हाम्रो पुर्खादेखि वर्तमान पुस्तासम्मको परिवारिक सम्बन्ध।
+            हाम्रो पुर्खादेखि वर्तमान पुस्तासम्मको पारिवारिक सम्बन्ध।
           </p>
+
         </div>
 
         <div className="tree">
+
           <ul>
             <TreeNode person={familyTree} />
           </ul>
+
         </div>
 
       </div>
+
     </section>
   );
 }
---
+++ src/data/familydata-temp.js
@@ -0,0 +1,45 @@
+export default [
+
+{
+id:1,
+parentId:null,
+name:"भेले पुन"
+},
+
+{
+id:2,
+parentId:1,
+name:"बाबू"
+},
+
+{
+id:3,
+parentId:1,
+name:"झुप्रो पुन"
+},
+
+{
+id:4,
+parentId:2,
+name:"जिजु बाजे"
+},
+
+{
+id:5,
+parentId:2,
+name:"बाबू"
+},
+
+{
+id:6,
+parentId:2,
+name:"बाबू"
+},
+
+{
+id:7,
+parentId:3,
+name:"बाजे"
+}
+
+];
